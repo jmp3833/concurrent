6557b67ae58854b11bfdf42340eee3a13e73b44b
amb8805 <amb8805@rit.edu>
2015-10-21 14:03:04 -0400

    PM

--- project/PM.java
+++ project/PM.java
@@ -1,8 +1,19 @@
 class PM extends Thread {
-
+  public long startTime;
+  public long endTime;
   public PM(String name) {
-    super(name);  }
+    super(name);}
+  
  
+  public void setTime(){
+      this.startTime = System.currentTimeMillis();
+      this.endTime = this.endTime + 5400;
+  }
+   
+  public long getTime(){
+      if(this.isAlive()) return(System.currentTimeMillis() - startTime);
+      else return endTime;
+  }
   public static void askPMQuestion(){
 	  try {
 		PM.sleep(100);
