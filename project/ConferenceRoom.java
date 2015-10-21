115079cb64729149bf297706614a45f893dad80e
Kevin Mulligan <kam9115@rit.edu>
2015-10-21 17:10:18 -0400

    Commented and cleaned up ConferenceRoom

--- project/ConferenceRoom.java
+++ project/ConferenceRoom.java
@@ -1,22 +1,26 @@
 class ConferenceRoom {
 
-  public boolean isFree;
-  public ConferenceRoom() {
-    this.isFree = true;
-  }
+    //Is this conference room currently in use
+    public boolean isFree;
 
-  public synchronized void holdMeeting() {
-    while(this.isFree == false) {
-      try {
-        wait();
-      } catch (InterruptedException e) {
-      }
+    public ConferenceRoom() {
+        this.isFree = true;
     }
-	this.isFree = false;
-  }
 
-  public synchronized void endMeeting() {
-	this.isFree = true;
-    notifyAll();
-  }
+    //Indicate that someone wants to hold a meeting, waiting until no one else is using it
+    public synchronized void holdMeeting() {
+        while(this.isFree == false) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+        }
+    }
+    this.isFree = false;
+    }
+
+    //End the current meeting, and let everyone else know it is empty
+    public synchronized void endMeeting() {
+        this.isFree = true;
+        notifyAll();
+    }
 }
