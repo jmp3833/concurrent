982f616b23a4f216177f1cd50bd06c4013353c6f
Justin Peterson <jmp3833@rit.edu>
2015-10-21 14:00:17 -0400

    Dev enters office between 8 and 8:30

--- project/Developer.java
+++ project/Developer.java
@@ -1,6 +1,34 @@
+import java.util.Random;
+
 class Developer extends Thread {
+
+  private Random rng;
 
   public Developer(String name) {
     super(name);
+    rng = new Random();
   }
+  
+  /*
+   * Conduct a standard day for the developer 
+   */
+  public void run() {
+
+    //Enter the office anywhere between 8 and 8:30 AM
+    try {
+      int arrivalMillis = rng.nextInt(290);
+      Thread.sleep(arrivalMillis);
+
+      String arrivalTime = (arrivalMillis / 10 < 10) ? 
+        ("0" + Integer.toString(arrivalMillis / 10)) : Integer.toString(arrivalMillis / 10);
+
+      System.out.println("Developer " + super.getName() + "enters the office at 8:"
+          + arrivalTime);
+    }
+
+    catch(InterruptedException e) {
+      e.printStackTrace(); 
+    }
+  }
+
 }
