b18b8cf9e038be36dfd6980fa997c9ddd2af5d22
Kevin Mulligan <kam9115@rit.edu>
2015-10-28 13:46:44 -0400

    Futures and ExecutorService

--- cgrep/CGrep.java
+++ cgrep/CGrep.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 import java.util.regex.Pattern;
 
 class CGrep {
@@ -12,6 +15,18 @@ class CGrep {
         String[] filenames = new String[args.length - 1];
         for(int i = 1; i < args.length; i++) {
             filenames[i - 1] = args [i]; 
+        }
+
+        //Make our executor service, with a threadpool of 3 and a queue to store other callables in.
+        ExecutorService executor = new ThreadPoolExecutor(3, 3, 60, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(args.length - 3));
+
+        //Array to store our futures in
+        ArrayList<Future<List<String>>> futures = new ArrayList();
+
+        for(String s : filenames) {
+            GrepTask gt = new GrepTask(s, reg);
+            Future<List<String>> f = executor.submit(gt);
+            futures.add(f);
         }
     }
 }
