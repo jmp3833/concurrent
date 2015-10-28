ec3e7e66360dae1fcd0ff0e19916f61ccf22e97a
Kevin Mulligan <kam9115@rit.edu>
2015-10-28 13:21:22 -0400

    Added argument checking and string->regex conversion

--- cgrep/CGrep.java
+++ cgrep/CGrep.java
@@ -2,7 +2,12 @@ import java.util.regex.Pattern;
 
 class CGrep {
     public static void main(String[] args) {
+        if(args.length < 2) {
+            System.err.println("Not enough arguments");
+            System.exit(1);
+        }
         String pattern = args[0];
+        Pattern reg = Pattern.compile(pattern);
 
         String[] filenames = new String[args.length - 1];
         for(int i = 1; i < args.length; i++) {
