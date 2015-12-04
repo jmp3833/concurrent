29ac933e9cac01ed02d86f896bf0e7339b14098a
Kevin Mulligan <kam9115@rit.edu>
2015-12-04 13:27:36 -0500

    Added bag and passenger objects

---
+++ project2/Bag.java
@@ -0,0 +1,11 @@
+public class Bag {
+    private Passenger pass;
+
+    public Passenger getPassenger() {
+        return pass;
+    }
+
+    public Bag(Passenger p) {
+        this.pass = p;
+    }
+}
---
+++ project2/Passenger.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Passenger {
+    String name;
+    List<Bag> bags;
+
+    public Passenger(String n, int numBags) {
+        this.name = n;
+        this.bags = new ArrayList<Bag>();
+        for(int i = 0; i < numBags; i++) {
+            this.bags.add(new Bag(this));
+        }
+    }
+}
