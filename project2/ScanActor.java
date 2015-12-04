ec41cfec1cdf9ae5ce9b828fe539dc6d1f9c0dc4
Justin Peterson <jmp3833@rit.edu>
2015-12-04 13:12:29 -0500

    Flesh out ScanActor

--- project2/ScanActor.java
+++ project2/ScanActor.java
@@ -8,45 +8,22 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 class ScanActor extends UntypedActor {
-
+  
   /*
-   * Construct a Found object to feed back to the 
-   * collection actor. The message object is of type Configure
-   * which contains the filename (or null for stdin), reference to
-   * the collection actor to notify on completion, and the regular
-   * expression object to match against.
+   * Recieve a ScanRequest object with a passenger and its cooresponding
+   * bag, or receive a message to shut down the system at the Scanner level.
    */
   public void onReceive(Object msg) {
 
-    Configure configure = (Configure) msg;
-    msg = (Configure) msg;
-    List<String> results = new ArrayList<String>();
-
-    //Append filename to the beginning of the list to tell them apart
-    results.add(configure.fileName);
-
-    try {
-      BufferedReader br = new BufferedReader(new FileReader(configure.fileName));
-      String line;
-
-      try {
-        while((line = br.readLine()) != null) {
-          //Build a pattern matcher on the regex 
-          Matcher m = configure.reg.matcher(line);
-          if(m.matches()) {
-            //Add line to the list if the pattern matches
-            results.add(line);
-          }
-        }
-      }
-      //Swallow buffer exception for now
-      catch(IOException e){
-      }
+    //Perform a Scan operation on the given user or baggage
+    if(msg instanceof ScanRequest) {
+    
     }
-    catch(FileNotFoundException e) {
+
+    //Shut the system down
+    else if(msg instanceof ShutdowmRequest) {
+    
     }
-    //Construct the message to the Configure actor
-    Found itemsFound = new Found(configure.fileName, results);
     configure.collection.tell(itemsFound);
   }
 } 
