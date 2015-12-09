082f9bbe404e2c39112741be52099d827091a786
Kevin Mulligan <kam9115@rit.edu>
2015-12-09 15:53:30 -0500

    Reworked scanner to have 2 messages: 1 for passenger and one for bag

--- project2/ScanActor.java
+++ project2/ScanActor.java
@@ -1,22 +1,25 @@
 import akka.actor.*;
 
 class ScanActor extends UntypedActor {
-  
-  /*
-   * Recieve a ScanRequest object with a passenger and its cooresponding
-   * bag, or receive a message to shut down the system at the Scanner level.
-   */
-  public void onReceive(Object msg) {
 
-    //Perform a Scan operation on the given user or baggage
-    if(msg instanceof ScanRequest) {
-    
-    }
+    /*
+     * Recieve a ScanRequest object with a passenger and its cooresponding
+     * bag, or receive a message to shut down the system at the Scanner level.
+     */
+    public void onReceive(Object msg) {
 
-    //Shut the system down
-    else if(msg instanceof ShutdownRequest) {
-    
+        //Perform a Scan operation on the given user or baggage
+        if (msg instanceof ScanPassengerRequest) {
+
+        }
+
+        if (msg instanceof ScanBagRequest) {
+        }
+
+        //Shut the system down
+        else if (msg instanceof ShutdownRequest) {
+
+        }
     }
-  }
 } 
 
